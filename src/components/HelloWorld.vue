731eec0305b67638de0f828721783d323b8e8778
uttues <458594033@qq.com>
2020-07-02 16:10:07 +0800

    7.2 轮播组件使用时传入height

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,39 @@
 <template>
-    <div class="frame">
-        <Swiper>
-            <SwiperItem v-for="(item, index) in 8"
-                        :key="index"
-                        :index="index" />
-        </Swiper>
-    </div>
+  <div class="frame">
+    <Swiper height='150px'>
+      <SwiperItem
+        v-for="(item, index) in 8"
+        :key="index"
+        :index="index"
+      />
+    </Swiper>
+  </div>
 </template>
 
 <script>
 import Swiper from "./swiper/swiper";
 import SwiperItem from "./swiper/swiper-item";
 export default {
-    components: {
-        Swiper,
-        SwiperItem
-    },
-    data() {
-        return {};
-    },
-    methods: {
-        update() {
-            console.log("hello");
-        }
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    update() {
+      console.log("hello");
     }
+  }
 };
 </script>
 
 <style scoped>
 .frame {
-    border: 1px solid;
-    width: 120px;
-    height: 200px;
-    margin: 0 auto;
+  border: 1px solid;
+  width: 120px;
+  height: 200px;
+  margin: 0 auto;
 }
 </style>
