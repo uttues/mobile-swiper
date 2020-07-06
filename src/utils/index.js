69d1db07065c091c57a5caf89ed622fbd31c821e
uttues <458594033@qq.com>
2020-07-06 21:28:47 +0800

    7.6 添加防抖节流工具函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,3 +12,76 @@ export const autoprefixer = function (style) {
     });
     return style;
 };
+
+
+/**
+ * @param {Number} callback 需要经过节流处理的函数
+ * @param {Number} delay 节流时长
+ * @param {boolean} [hasTailing] 在节流最后是否多调用一次，默认无
+ * 
+ * @return {Function} 调用返回的函数即有节流效果
+ */
+export const throttle = function (callback, delay, hasTailing = false) {
+    let timeoutID = undefined
+    let previous = 0; // 该节流函数上一次被执行的时间戳（初始化为0，则第一次一定可以执行）
+
+    return function () {
+        const self = this
+        const args = arguments
+        let passedTime = new Date() - previous
+
+        function exec() {
+            previous = new Date()
+            callback.call(self, ...args) // 相比于apply，call的性能会更好一些~
+        }
+
+        if (passedTime >= delay) {
+            exec()
+        } else {
+            // 如果已经有timeoutID，表明已经有定时器准备处理“尾巴”了，无需重建
+            if (hasTailing && !timeoutID) {
+                timeoutID = setTimeout(() => {
+                    exec()
+                    timeoutID = undefined
+                }, delay - passedTime)
+            }
+        }
+    }
+}
+
+
+/**
+ * @param {Number} callback 需要经过节流处理的函数
+ * @param {Number} delay 节流时长
+ * @param {boolean} [atBegin] 是否在首次调用时就执行该函数，默认为false
+ *
+ * @return {Function} 调用返回的函数即有节流效果
+ */
+export const debounce = function (callback, delay, atBegin = false) {
+    let timeoutID = null
+
+    return function () {
+        const self = this
+        const args = arguments
+
+        if (atBegin) {
+            if (!timeoutID) {
+                callback.call(self, ...args)
+            } else {
+                clearTimeout(timeoutID)
+            }
+            timeoutID = setTimeout(function () {
+                timeoutID = undefined
+            }, delay)
+        } else {
+            if (timeoutID) {
+                clearTimeout(timeoutID)
+            }
+            timeoutID = setTimeout(function () {
+                callback.call(self, ...args)
+                timeoutID = undefined
+            }, delay)
+        }
+    }
+}
+
