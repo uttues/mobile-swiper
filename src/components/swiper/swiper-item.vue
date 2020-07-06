b98b65f795c52d94630d479a4fc8aede8868d4cd
uttues <458594033@qq.com>
2020-07-06 12:40:11 +0800

    7.6 代码梳理

--- src/components/swiper/swiper-item.vue
+++ src/components/swiper/swiper-item.vue
@@ -1,139 +1,152 @@
 <template>
-  <div
-    class="swiper-item"
-    :class="{ 
+    <div
+        class="swiper-item"
+        :class="{ 
         'animating': isAnimating,
         'touching': isTouching
         }"
-    :style="itemStyle"
-  >
-    {{ index }}
-    <slot></slot>
-  </div>
+        :style="itemStyle"
+    >
+        <slot></slot>
+    </div>
 </template>
 
 <script>
 import { autoprefixer } from "../../utils";
 export default {
-  name: "SwiperItem",
-  data() {
-    return {
-      // translate: 移动距离，改变时触发生成新的样式（动态样式）
-      // inStage：该元素是否处于边缘状态
-      // isAnimating: 决定是否添加动作类animating
-      // isAnimating: 决定是否添加动作类touching
-      // autoAnimDuration: 一次轮播图片所用的时间，默认500，在父组件那边设置默认值
-      // beforeTouchX：touch事件触发前该元素的translate值
-      translate: 0,
-      inStage: false,
-      isAnimating: false,
-      isTouching: false,
-      beforeTouchX: 0,
-    };
-  },
-  computed: {
-    /** 
-     * 获取父组件中的autoAnimDuration，及时更新
-    */
-    autoAnimDuration(){
-      return this.$parent.autoAnimDuration
+    name: "SwiperItem",
+    data() {
+        return {
+            // translate: 移动距离，改变时触发生成新的样式（动态样式）
+            // inStage：该元素是否处于边缘状态
+            // isAnimating: 决定是否添加动作类animating
+            // isAnimating: 决定是否添加动作类touching
+            // autoAnimDuration: 一次轮播图片所用的时间，默认500，在父组件那边设置默认值
+            // beforeTouchX：touch事件触发前该元素的translate值
+            translate: 0,
+            inStage: false,
+            isAnimating: false,
+            isTouching: false,
+            beforeTouchX: 0
+        };
     },
-    itemsCount(){
-      return this.$parent.items.length
-    },
+    computed: {
+        /**
+         * 获取父组件中的autoAnimDuration，及时更新
+         */
+        autoAnimDuration() {
+            return this.$parent.autoAnimDuration;
+        },
+        itemsCount() {
+            return this.$parent.items.length;
+        },
 
-    /**
-     * translate的值发生改变就会自动执行，计算样式，返回一个style对象，动态样式
-     */
-    itemStyle() {
-      const transitionValue = this.isAnimating || this.isTouching 
-                            ? `transform ${this.autoAnimDuration / 1000}s ease-in-out`
-                            : `none`
-      const style = {
-        transform: `translateX(${this.translate}px)`,
-        transition: transitionValue
-      };
-      return autoprefixer(style);
+        /**
+         * translate的值发生改变就会自动执行，计算样式，返回一个style对象，动态样式
+         */
+        itemStyle() {
+            const transitionValue =
+                this.isAnimating || this.isTouching
+                    ? `transform ${this.autoAnimDuration / 1000}s ease-in-out`
+                    : `none`;
+            const style = {
+                transform: `translateX(${this.translate}px)`,
+                transition: transitionValue
+            };
+            return autoprefixer(style);
+        }
     },
-  },
-  methods: {
-    // 返回当前元素处理后的index值，用于计算新的translate值
-    processIndex(index, activeIndex) {
-      const length = this.itemsCount
-      if (activeIndex === 0 && index === length - 1) {
-        return -1;
-      } else if (activeIndex === length - 1 && index === 0) {
-        return length;
-      }
-      return index;
-    },
-    
-    updateTranslate(index, activeIndex) {
-      // offsetWidth = width + 左右padding + 左右boder
-      const distance = this.$parent.$el["offsetWidth"];
-      return distance * (index - activeIndex);
-    },
+    methods: {
+        /**
+         * 返回当前元素处理后的index值，用于计算新的translate值
+         */
+        processIndex(index, activeIndex) {
+            const length = this.itemsCount;
+            if (activeIndex === 0 && index === length - 1) {
+                return -1;
+            } else if (activeIndex === length - 1 && index === 0) {
+                return length;
+            }
+            return index;
+        },
 
-    /**
-     * 需要滑动时，由外层 swiper 调用slideTranslateItem，触发元素移动
-     * @param index 当前swiper-item索引
-     * @param activeIndex 轮播需要显示在正中间的swiper-item索引
-     * @param oldIndex 过去的activeIndex
-     * 所有元素都会重新计算自己的translate值，生成动态样式
-     * (列表中只有两个元素需要移动，旧的activeIndex（左移移走），新的activeIndex（左移移入）？？？)
-     */
-    slideTranslateItem(index, activeIndex, oldIndex) {
-      // isAnimating 两种情况
-      //  ● 主角 
-      //  ● 处于两主角中间，并且不是 itemscount - 1 => 0 
-      this.isAnimating = (index === oldIndex || index === activeIndex)
-        || (((oldIndex < index && index < activeIndex) || (activeIndex < index && index < oldIndex)) 
-        && ((oldIndex !== this.itemsCount - 1) || (activeIndex !== 0))
-        && ((oldIndex !== 0) || (activeIndex !== this.itemsCount - 1)));
-      
-      // 处理当前索引
-      index = this.processIndex(index, activeIndex, this.itemsCount);
+        /**
+         * 更新当前元素的transition，触发生成动态样式
+         */
+        updateTranslate(index, activeIndex) {
+            // offsetWidth = width + 左右padding + 左右boder
+            const distance = this.$parent.$el["offsetWidth"];
+            return distance * (index - activeIndex);
+        },
 
-      // 计算当前元素的Translate并修改 => 触发新的style计算，动态样式
-      this.translate = this.updateTranslate(index, activeIndex);
-      // this.ready = true;
-    },
-    
-    toucherStart() {
-      this.isAnimating = false;
-      this.beforeTouchX = this.translate;
-    },
-    toucherTranslateItem(index, activeIndex, dragDistance) {
-      // if (index !== activeIndex && itemsCount > 2) {
-      // 处理当前索引
-      // 如果不执行这一个processIndex，则不会实现循环播放
-      index = this.processIndex(index, activeIndex);
-      
-      // 相邻的 Math.abs(index - activeIndex) <= 1 或者 同为列表边界
-      this.isTouching = Math.abs(index - activeIndex) <= 1
-      // }
-      if (this.isTouching) {
-        this.translate = this.beforeTouchX + dragDistance;
-      }
-    },
-    toucherEnd() {
-      // this.isAnimating = true;
-      this.isTouching = false;
+        /**
+         * 需要滑动时，由外层 swiper 调用slideTranslateItem，触发元素移动
+         * @param index 当前swiper-item索引
+         * @param activeIndex 轮播需要显示在正中间的swiper-item索引
+         * @param oldIndex 过去的activeIndex
+         * 所有元素都会重新计算自己的translate值，生成动态样式
+         * (列表中只有两个元素需要移动，旧的activeIndex（左移移走），新的activeIndex（左移移入）？？？)
+         */
+        slideTranslateItem(index, activeIndex, oldIndex) {
+            // isAnimating 两种情况
+            //  ● 主角
+            //  ● 处于两主角中间，并且不是 itemscount - 1 => 0
+            this.isAnimating =
+                index === oldIndex ||
+                index === activeIndex ||
+                (((oldIndex < index && index < activeIndex) ||
+                    (activeIndex < index && index < oldIndex)) &&
+                    (oldIndex !== this.itemsCount - 1 || activeIndex !== 0) &&
+                    (oldIndex !== 0 || activeIndex !== this.itemsCount - 1));
+
+            // 处理当前索引
+            index = this.processIndex(index, activeIndex, this.itemsCount);
+
+            // 计算当前元素的Translate并修改 => 触发新的style计算，动态样式
+            this.translate = this.updateTranslate(index, activeIndex);
+            // this.ready = true;
+        },
+
+        /**
+         *  处理元素下标，更新translate（实现元素拖拽跟随）
+         */
+        toucherTranslateItem(index, activeIndex, dragDistance) {
+            // 如果不执行这一个processIndex，则不会实现循环播放
+            index = this.processIndex(index, activeIndex);
+
+            if (this.isTouching) {
+                this.translate = this.beforeTouchX + dragDistance;
+            }
+        },
+
+        /**
+         * 修改元素动态类（状态）
+         */
+        toucherStart(index, activeIndex) {
+            this.isAnimating = false;
+            // 相邻的 Math.abs(index - activeIndex) <= 1 或者 同为列表边界
+            this.isTouching = Math.abs(index - activeIndex) <= 1;
+            this.beforeTouchX = this.translate;
+        },
+
+        /**
+         * 修改元素动态类（状态），在父组件处理touchEnd时会重新开启计时器（轮播），所以这边无需处理isAnimating
+         */
+        toucherEnd() {
+            this.isTouching = false;
+        }
     }
-  },
-  mounted() {
-  }
 };
 </script>
 
 <style scope>
 .swiper-item {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: #4962;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #4962;
 }
 .touching {
-  transition: transform 0;
+    transition: transform 0;
 }
 </style>
--- src/components/swiper/swiper.vue
+++ src/components/swiper/swiper.vue
@@ -1,316 +1,329 @@
 <template>
-  <div class="swiper">
-    <div
-      class="swiper-container"
-      :style="{ height: height }"
-      @touchstart="handleTouchStart"
-      @touchmove="handleTouchMove"
-      @touchend="handleTouchEnd"
-    >
-      <slot></slot>
-      <button class="swiper-arrow" @click="prev"><i>《=</i></button>
-      <button class="swiper-arrow" @click="next"><i>=》</i></button>
+    <div class="swiper">
+        <div
+            class="swiper-container"
+            :style="{ height: height }"
+            @touchstart="handleTouchStart"
+            @touchmove="handleTouchMove"
+            @touchend="handleTouchEnd"
+        >
+            <slot></slot>
+            <button
+                class="swiper-arrow"
+                @click="prev"
+            ><i>《=</i></button>
+            <button
+                class="swiper-arrow"
+                @click="next"
+            ><i>=》</i></button>
+        </div>
+        <ul class="swiper-indicators">
+            <li
+                class="swiper-indicator"
+                :class="{'swiper-indicator-active': activeIndex === index}"
+                v-for="(item, index) in items"
+                :key="`swiper-indicator-${index}`"
+                @click="indicatorClick(index)"
+            >
+            </li>
+        </ul>
     </div>
-    <ul class="swiper-indicators">
-      <li 
-        class="swiper-indicator" 
-        :class="{'swiper-indicator-active': activeIndex === index}"
-        v-for="(item, index) in items" 
-        :key="`swiper-indicator-${index}`" 
-        @click="indicatorClick(index)">
-      </li>
-    </ul>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "Swiper",
-  props: {
-    // interval：自动轮播间隔时长
-    // slideDuration: 完整轮播（滑动）一次所花时间
-    // height：绝对定位，宽度撑满，高度定制（如果不传入高度会塌陷）
-    // dragRatioMinLimit：拖拽超过这个限度就会触发轮播
-    interval: {
-      type: Number,
-      default: 3000
-    },
-    slideDuration:{
-      type: Number,
-      default: 500
-    },
-    height: {
-      type: String,
-      default: "300px",
-      required: true
-    },
-    // 
-    dragRatioMinLimit:{
-      type:Number,
-      default:0.25
-    }
-  },
-  data() {
-    return {
-      // items：swiper-item列表（DOM元素列表，用于：父子组件通信，长度渲染指示器）
-      // initialIndex：暂时不接受指定，默认0，后期可以作为prop让外界决定
-      // activeIndex：当前处于展示区的swiper-item索引
-      // timer：定时器
-
-      // 自动滑动相关：定时器轮播、按钮切换、指示器切换、拖拽引起的小滑动
-      // autoAnimDuration：一次自动滑动所需时间（初始值由父组件传入，默认500）
-      //  ● 只有在拖拽相关时需要修改，其它情况均用默认传入的slideDuration
-      // isAutoSliding: 当触发的自动滑动正在进行时，拖拽无效
-      
-      // 拖拽事件：
-      // startTouchX: touchStart时的横坐标
-      // dragDistance: 最近一次拖拽终点到拖拽起点的水平距离 
-      items: [],
-      initialIndex: 0, 
-      activeIndex: -1,
-      timer: null,
-      
-      autoAnimDuration: 0,
-      isAutoSliding: false,
-      
-      startTouchX: 0,
-      dragDistance: 0
-    };
-  },
-  watch: {
-    activeIndex(val, oldVal) {
-      this.resetItemsPosition(oldVal);
-    }
-  },
-  methods: {
-    /** 
-     * 维护 isAutoSliding 状态，调用时即刻开启自动滑动状态，滑动时间后关闭
-     *  ● 无需传参：直接用维护的autoAnimDuration
-     *  ● 注意：先更新autoAnimDuration后才能触发滑动
-    */
-    setAutoSlideStatus() {
-      this.isAutoSliding = true
-      setTimeout(()=>{
-        this.isAutoSliding = false
-      }, this.autoAnimDuration)
-    },
-
-    /** 
-     * 维护 autoAnimDuration 状态（拖拽+尾滑完成后恢复原值slideDuration）
-     * @param value 
-     * @param isReset 是否需要延时重置，拖拽进行touchMove中不需要，但拖拽后引起的滑动需要重置（默认不需要）
-    */
-    setAutoAnimDuration(value, isReset = false) {
-      const oldValue = this.autoAnimDuration
-      this.autoAnimDuration = value
-      if (isReset) {
-        setTimeout(()=>{
-          this.autoAnimDuration = this.duration
-        }, oldValue)
-      }
-    },
-
-    /** 
-     * 轮播轨道滑动，本质上调用setActiveItem修改值（自动 开启滑动保护，setAutoSlideStatus）
-     *  ● 适用情况：定时器轮播、按钮切换、指示器切换、拖拽引起的小滑动
-     *  ● 注意事项：调用之前需确定自动滑动时间，autoAnimDuration
-     * @param offset 正值表示：展示列表后第n张，负值表示：展示列表前第n张
-    */
-    playSlide(offset) {
-      this.setAutoSlideStatus()
-      this.setActiveItem(this.activeIndex + offset)
-      this.delayRestartTimer(this.autoAnimDuration)
-    },
-    
-    /** 
-     * 开启定时器
-    */
-    startTimer() {
-      if (this.interval <= 0 || this.timer) return;
-      this.timer = setInterval(this.playSlide, this.interval, 1);
-    },
-
-    /** 
-     * 关闭定时器
-    */
-    pauseTimer() {
-      if (this.timer) {
-        clearInterval(this.timer);
-        this.timer = null;
-      }
-    },
-
-    /** 
-     * 非自然滑动（按钮、指示器、拖拽小滑动）需要重启定时器，多数情况interval就是this.autoAnimDuration
-    */
-    delayRestartTimer(interval) {
-      this.pauseTimer()
-      setTimeout(this.startTimer, interval)
-    },
-    
-    /**
-     * 根据子组件的name属性，更新 swiper-item 组件列表items
-     */
-    updateItems() {
-      this.items = this.$children.filter(
-        child => child.$options.name === "SwiperItem"
-      );
-      this.setActiveItem(this.initialIndex);
-    },
-
-    /** 
-     * 调用每个swiper-item组件的 slideTranslateItem，触发元素移动
-     * （负责定时器、按钮类型的轮播）
-    */
-    resetItemsPosition(oldIndex) {
-      console.log('resetItemsPosition');
-      this.items.forEach((item, index) => {
-        item.slideTranslateItem(index, this.activeIndex, oldIndex);
-      });
-    },
-
-    /**
-     * 修改this.activeIndex的值，对边界做了处理
-     */
-    setActiveItem(index) {
-      const oldIndex = this.activeIndex;
-
-      if(index < 0) {
-        this.activeIndex = this.items.length - 1
-      } else if(index > this.items.length - 1) {
-        this.activeIndex = 0;
-      } else {
-        this.activeIndex = index
-      }
-      
-      // 如果index===activeIndex，不会触发watch
-      if (this.activeIndex === oldIndex) {
-        this.resetItemsPosition(oldIndex);
-      }
-    },
-    
-
-    /** 
-     * 按钮滑动：前一个swiper-item
-    */
-    prev() {
-      this.playSlide(-1)
-    },
-    /** 
-     * 按钮滑动：下一个swiper-item
-    */
-    next() {
-      this.playSlide(1)
-    },
-
-    /** 
-     * 指示器切换滑动：点击下标为index的 swiper-item
-    */
-    indicatorClick(index) {
-      this.playSlide(index - this.activeIndex)
-    },
-
-
-    /** 
-     * 一些统筹操作，通知子组件修改状态（isTouching）
-    */
-    handleTouchStart(event) {
-      if(this.isAutoSliding) return
-
-      this.startTouchX = event.touches[0].pageX;
-
-      // 让子组件跟随鼠标快速移动
-      this.setAutoAnimDuration(0, false)
-
-      this.pauseTimer();
-      
-      this.items.forEach(item => {
-        item.toucherStart();
-      });
-    },
-
-    /** 
-     * 一些统筹操作，通知子组件跟随
-    */
-    handleTouchMove(event) {
-      if(this.isAutoSliding) return
-
-      this.dragDistance = event.touches[0].pageX - this.startTouchX;
-
-      this.items.forEach((item, index) => {
-        item.toucherTranslateItem(index, this.activeIndex, this.dragDistance);
-      });
-    },
-
-    /** 
-     * Touch结束时触发，需要处理的事情
-     * 1、计算拖动的比例，判断是否需要引起轮播
-     * 2、除了拖动比例为0之外，都需要引起一次 距离<width,时间<autoAnimDuration 的小滑动
-     *    2.1、修改 autoAnimDuration 的值
-     *    2.2、触发子组件计算位置（activeIndex变动触发||手动调用reset触发） => 用setActiveIndex
-     *    2.3、定时器修改 autoAnimDuration 的值为原先组件外传入的值
-     * 3、拖动结束后开启计时器
-    */
-    handleTouchEnd() {
-      if(this.isAutoSliding) return
-
-      const dragRatio = Math.abs(this.dragDistance / this.$el['offsetWidth'])
-
-       // dragRatio>0 会触发小滑动 => 设置滑动时间，开启滑动（内部自动设置滑动保护、定时器重置操作）
-      if(dragRatio === 0) {
-        this.delayRestartTimer(0) 
-      } else if (dragRatio < this.dragRatioMinLimit) {
-        this.setAutoAnimDuration(this.slideDuration * dragRatio, true)
-        this.playSlide(0)
-      } else {
-        // 修改滑动时间、开启滑动playSlide的顺序不能错
-        this.setAutoAnimDuration(this.slideDuration * (1 - dragRatio), true)
-        if(this.dragDistance > 0) {
-          this.playSlide(-1)
-        } else {
-          this.playSlide(1)
+    name: "Swiper",
+    props: {
+        // interval：自动轮播间隔时长
+        // slideDuration: 完整轮播（滑动）一次所花时间
+        // height：绝对定位，宽度撑满，高度定制（如果不传入高度会塌陷）
+        // dragRatioMinLimit：拖拽超过这个限度就会触发轮播
+        interval: {
+            type: Number,
+            default: 3000
+        },
+        slideDuration: {
+            type: Number,
+            default: 500
+        },
+        height: {
+            type: String,
+            default: "300px",
+            required: true
+        },
+        //
+        dragRatioMinLimit: {
+            type: Number,
+            default: 0.25
         }
-      }
+    },
+    data() {
+        return {
+            // items：swiper-item列表（DOM元素列表，用于：父子组件通信，长度渲染指示器）
+            // initialIndex：暂时不接受指定，默认0，后期可以作为prop让外界决定
+            // activeIndex：当前处于展示区的swiper-item索引
+            // timer：定时器
 
-      // 子组件修改自己的isTouching状态
-      this.items.forEach(item => {
-        item.toucherEnd();
-      });
+            // 自动滑动相关：定时器轮播、按钮切换、指示器切换、拖拽引起的小滑动
+            // autoAnimDuration：一次自动滑动所需时间（初始值由父组件传入，默认500）
+            //  ● 只有在拖拽相关时需要修改，其它情况均用默认传入的slideDuration
+            // isAutoSliding: 当触发的自动滑动正在进行时，拖拽无效
+
+            // 拖拽事件：
+            // startTouchX: touchStart时的横坐标
+            // dragDistance: 最近一次拖拽终点到拖拽起点的水平距离
+            items: [],
+            initialIndex: 0,
+            activeIndex: -1,
+            timer: null,
+
+            autoAnimDuration: 0,
+            isAutoSliding: false,
+
+            startTouchX: 0,
+            dragDistance: 0
+        };
+    },
+    watch: {
+        activeIndex(val, oldVal) {
+            this.resetItemsPosition(oldVal);
+        }
+    },
+    methods: {
+        /**
+         * 维护 isAutoSliding 状态，调用时即刻开启自动滑动状态，滑动时间后关闭
+         *  ● 无需传参：直接用维护的autoAnimDuration
+         *  ● 注意：先更新autoAnimDuration后才能触发滑动
+         */
+        setAutoSlideStatus() {
+            this.isAutoSliding = true;
+            setTimeout(() => {
+                this.isAutoSliding = false;
+            }, this.autoAnimDuration);
+        },
+
+        /**
+         * 维护 autoAnimDuration 状态（拖拽+尾滑完成后恢复原值slideDuration）
+         * @param value
+         * @param isReset 是否需要延时重置，拖拽进行touchMove中不需要，但拖拽后引起的滑动需要重置（默认不需要）
+         */
+        setAutoAnimDuration(value, isReset = false) {
+            const oldValue = this.autoAnimDuration;
+            this.autoAnimDuration = value;
+            if (isReset) {
+                setTimeout(() => {
+                    this.autoAnimDuration = this.duration;
+                }, oldValue);
+            }
+        },
+
+        /**
+         * 轮播轨道滑动，本质上调用setActiveItem修改值（自动 开启滑动保护，setAutoSlideStatus）
+         *  ● 适用情况：定时器轮播、按钮切换、指示器切换、拖拽引起的小滑动
+         *  ● 注意事项：调用之前需确定自动滑动时间，autoAnimDuration
+         * @param offset 正值表示：展示列表后第n张，负值表示：展示列表前第n张
+         */
+        playSlide(offset) {
+            this.setAutoSlideStatus();
+            this.setActiveItem(this.activeIndex + offset);
+            this.delayRestartTimer(this.autoAnimDuration);
+        },
+
+        /**
+         * 开启定时器
+         */
+        startTimer() {
+            if (this.interval <= 0 || this.timer) return;
+            this.timer = setInterval(this.playSlide, this.interval, 1);
+        },
+
+        /**
+         * 关闭定时器
+         */
+        pauseTimer() {
+            if (this.timer) {
+                clearInterval(this.timer);
+                this.timer = null;
+            }
+        },
+
+        /**
+         * 非自然滑动（按钮、指示器、拖拽小滑动）需要重启定时器，多数情况interval就是this.autoAnimDuration
+         */
+        delayRestartTimer(interval) {
+            this.pauseTimer();
+            setTimeout(this.startTimer, interval);
+        },
+
+        /**
+         * 根据子组件的name属性，更新 swiper-item 组件列表items
+         */
+        updateItems() {
+            this.items = this.$children.filter(
+                child => child.$options.name === "SwiperItem"
+            );
+            this.setActiveItem(this.initialIndex);
+        },
+
+        /**
+         * 调用每个swiper-item组件的 slideTranslateItem，触发元素移动
+         * （负责定时器、按钮类型的轮播）
+         */
+        resetItemsPosition(oldIndex) {
+            console.log("resetItemsPosition");
+            this.items.forEach((item, index) => {
+                item.slideTranslateItem(index, this.activeIndex, oldIndex);
+            });
+        },
+
+        /**
+         * 修改this.activeIndex的值，对边界做了处理
+         */
+        setActiveItem(index) {
+            const oldIndex = this.activeIndex;
+
+            if (index < 0) {
+                this.activeIndex = this.items.length - 1;
+            } else if (index > this.items.length - 1) {
+                this.activeIndex = 0;
+            } else {
+                this.activeIndex = index;
+            }
+
+            // 如果index===activeIndex，不会触发watch
+            if (this.activeIndex === oldIndex) {
+                this.resetItemsPosition(oldIndex);
+            }
+        },
+
+        /**
+         * 按钮滑动：前一个swiper-item
+         */
+        prev() {
+            this.playSlide(-1);
+        },
+        /**
+         * 按钮滑动：下一个swiper-item
+         */
+        next() {
+            this.playSlide(1);
+        },
+
+        /**
+         * 指示器切换滑动：点击下标为index的 swiper-item
+         */
+        indicatorClick(index) {
+            this.playSlide(index - this.activeIndex);
+        },
+
+        /**
+         * 一些统筹操作，通知子组件修改状态（isTouching）
+         */
+        handleTouchStart(event) {
+            if (this.isAutoSliding) return;
+
+            this.startTouchX = event.touches[0].pageX;
+
+            // 让子组件跟随鼠标快速移动
+            this.setAutoAnimDuration(0, false);
+
+            this.pauseTimer();
+
+            this.items.forEach((item, index) => {
+                item.toucherStart(index, this.activeIndex);
+            });
+        },
+
+        /**
+         * 一些统筹操作，通知子组件跟随
+         */
+        handleTouchMove(event) {
+            if (this.isAutoSliding) return;
+
+            this.dragDistance = event.touches[0].pageX - this.startTouchX;
+
+            this.items.forEach((item, index) => {
+                item.toucherTranslateItem(
+                    index,
+                    this.activeIndex,
+                    this.dragDistance
+                );
+            });
+        },
+
+        /**
+         * Touch结束时触发，需要处理的事情
+         * 1、计算拖动的比例，判断是否需要引起轮播
+         * 2、除了拖动比例为0之外，都需要引起一次 距离<width,时间<autoAnimDuration 的小滑动
+         *    2.1、修改 autoAnimDuration 的值
+         *    2.2、触发子组件计算位置（activeIndex变动触发||手动调用reset触发） => 用setActiveIndex
+         *    2.3、定时器修改 autoAnimDuration 的值为原先组件外传入的值
+         * 3、拖动结束后开启计时器
+         */
+        handleTouchEnd() {
+            if (this.isAutoSliding) return;
+
+            const dragRatio = Math.abs(
+                this.dragDistance / this.$el["offsetWidth"]
+            );
+
+            // dragRatio>0 会触发小滑动 => 设置滑动时间，开启滑动（内部自动设置滑动保护、定时器重置操作）
+            if (dragRatio === 0) {
+                this.delayRestartTimer(0);
+            } else if (dragRatio < this.dragRatioMinLimit) {
+                this.setAutoAnimDuration(this.slideDuration * dragRatio, true);
+                this.playSlide(0);
+            } else {
+                // 修改滑动时间、开启滑动playSlide的顺序不能错
+                this.setAutoAnimDuration(
+                    this.slideDuration * (1 - dragRatio),
+                    true
+                );
+                if (this.dragDistance > 0) {
+                    this.playSlide(-1);
+                } else {
+                    this.playSlide(1);
+                }
+            }
+
+            // 子组件修改自己的isTouching状态
+            this.items.forEach(item => {
+                item.toucherEnd();
+            });
+        }
     },
 
-  },
-
-  mounted() {
-    this.$nextTick(() => {
-      // 让prop初始化data，而autoAnimDuration在后期会因为各种操作而修改
-      this.autoAnimDuration = this.slideDuration
-      this.updateItems();
-      this.startTimer();
-    });
-  },
-  beforeDestroy() {
-    this.pauseTimer();
-  }
+    mounted() {
+        this.$nextTick(() => {
+            // 让prop初始化data，而autoAnimDuration在后期会因为各种操作而修改
+            this.autoAnimDuration = this.slideDuration;
+            this.updateItems();
+            this.startTimer();
+        });
+    },
+    beforeDestroy() {
+        this.pauseTimer();
+    }
 };
 </script>
 
 <style scope>
 .swiper {
-  position: relative;
+    position: relative;
 }
 .swiper-container {
-  position: relative;
-  overflow: hidden;
-  height: 100%;
+    position: relative;
+    /* overflow: hidden; */
+    height: 100%;
 }
 .swiper-indicator {
-  display: inline-block;
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
-  background-color: #f00;
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #f00;
 }
 .swiper-indicator-active {
-  background-color: #0f0;
+    background-color: #0f0;
 }
 </style>
