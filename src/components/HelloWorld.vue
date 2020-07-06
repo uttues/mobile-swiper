75d781e8d7e61a759a028a1d38b6191a2a71f43c
uttues <458594033@qq.com>
2020-07-06 13:47:00 +0800

    7.6 添加“按钮如何显示”的控制变量（不显示、一直显示、悬浮显示）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,43 @@
 <template>
-  <div class="frame">
-    <Swiper height='150px' :slide-duration="500" :interval="2000">
-      <SwiperItem
-        v-for="(item, index) in 8"
-        :key="index"
-      >{{index}}</SwiperItem>
-    </Swiper>
-  </div>
+    <div class="frame">
+        <Swiper
+            height='150px'
+            :slide-duration="500"
+            :interval="2000"
+            show-arrow-type="always"
+        >
+            <SwiperItem
+                v-for="(item, index) in 8"
+                :key="index"
+            >{{index}}</SwiperItem>
+        </Swiper>
+    </div>
 </template>
 
 <script>
 import Swiper from "./swiper/swiper";
 import SwiperItem from "./swiper/swiper-item";
 export default {
-  components: {
-    Swiper,
-    SwiperItem
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    update() {
-      console.log("hello");
+    components: {
+        Swiper,
+        SwiperItem
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        update() {
+            console.log("hello");
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 .frame {
-  border: 1px solid;
-  width: 200px;
-  height: 200px;
-  margin: 0 auto;
+    border: 1px solid;
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
 }
 </style>
