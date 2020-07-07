b11780c3ea4f9de7c97d85104b016dbc1549df28
uttues <458594033@qq.com>
2020-07-07 10:16:14 +0800

    7.7 防抖节流 ES6 化

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,22 +17,20 @@ export const autoprefixer = function (style) {
 /**
  * @param {Number} callback 需要经过节流处理的函数
  * @param {Number} delay 节流时长
- * @param {boolean} [hasTailing] 在节流最后是否多调用一次，默认无
+ * @param {boolean} [hasTailing] 默认false，在节流最后是否多调用一次（被节流的函数在最后节流结束后重新调用）
  * 
  * @return {Function} 调用返回的函数即有节流效果
  */
 export const throttle = function (callback, delay, hasTailing = false) {
-    let timeoutID = undefined
+    let timeoutID
     let previous = 0; // 该节流函数上一次被执行的时间戳（初始化为0，则第一次一定可以执行）
 
     return function () {
-        const self = this
-        const args = arguments
         let passedTime = new Date() - previous
 
-        function exec() {
+        const exec = () => {
             previous = new Date()
-            callback.call(self, ...args) // 相比于apply，call的性能会更好一些~
+            callback.call(this, ...arguments) // 相比于apply，call的性能会更好一些~
         }
 
         if (passedTime >= delay) {
@@ -61,12 +59,10 @@ export const debounce = function (callback, delay, atBegin = false) {
     let timeoutID = null
 
     return function () {
-        const self = this
-        const args = arguments
 
         if (atBegin) {
             if (!timeoutID) {
-                callback.call(self, ...args)
+                callback.call(this, ...arguments)
             } else {
                 clearTimeout(timeoutID)
             }
@@ -78,7 +74,7 @@ export const debounce = function (callback, delay, atBegin = false) {
                 clearTimeout(timeoutID)
             }
             timeoutID = setTimeout(function () {
-                callback.call(self, ...args)
+                callback.call(this, ...arguments)
                 timeoutID = undefined
             }, delay)
         }
